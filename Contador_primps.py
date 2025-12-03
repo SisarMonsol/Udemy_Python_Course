8d51f911ddbeb327eeb8da3d4a2ab48197e91634
Cesar Montanez <cmontanezsolorzano@gmail.com>
2025-12-02 21:53:44 -0600

    Cree la base del contador
    Desarrollé lo que será la base del programa de conteo de números primos.
    Aún falta terminar la función que identificará a los números primos.

---
+++ Contador_primps.py
@@ -0,0 +1,12 @@
+#Este programa recibirá un número (rango máximo) y contará todos los números primos que se encuentren dentro de ese rango.
+#Este conteo va desde el 0 hasta el número incluído, pero el 0 y el 1 no se consideran como primos.
+
+def contar_primos(numero):
+   
+  for num in range(numero):
+     
+  return conteo
+
+numero = int(input('Ingresa un número: '))
+cantidad = contar_primos(numero)
+print(f'Desde el 0 hasta el {numero} hay {cantidad} números primos.')
