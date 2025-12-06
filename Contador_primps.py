c1d23c381cb43e63d33ed1ec3dc46573dd074833
Cesar Montanez <cmontanezsolorzano@gmail.com>
2025-12-06 11:31:16 -0600

    Refactor prime counting logic and add helper function
    El código optimiza el conteo sin revisar número por número, solo los necesarios. Ya quedó terminado.

--- Contador_primps.py
+++ Contador_primps.py
@@ -1,12 +1,22 @@
 #Este programa recibirá un número (rango máximo) y contará todos los números primos que se encuentren dentro de ese rango.
 #Este conteo va desde el 0 hasta el número incluído, pero el 0 y el 1 no se consideran como primos.
 
-def contar_primos(numero):
-   
-  for num in range(numero):
-     
-  return conteo
+def es_primo(numero):
+   if numero <= 1: #Eliminamos el 0 y 1 porque no son primos
+      return False
+   for i in range(2, int(numero**0.5) + 1): #buscamos si el número tiene otro divisor además de él mismo y el 1
+      if numero % i == 0: #si el divisor no deja un residuo (un decimal) entonces el número no es primo
+         return False
+   return True
 
-numero = int(input('Ingresa un número: '))
-cantidad = contar_primos(numero)
-print(f'Desde el 0 hasta el {numero} hay {cantidad} números primos.')
+def contar_primos(limite):
+  contador = 0
+
+  for num in range(2, limite + 1): 
+     if es_primo(num):
+        contador += 1
+  return contador
+
+limite = int(input('Ingresa el límite hasta el que hay que contar: '))
+cantidad = contar_primos(limite)
+print(f'Desde el 0 hasta el {limite} hay {cantidad} números primos.')
